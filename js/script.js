d6f924614e12f3fb7ad8ad823139450bf6a5400b
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-06 11:21:21 -0800

    v.2.4
    Still working with JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var p1Click = document.querySelector("#p1");
 var p2Click = document.getElementById("p2");
-var resetClick = document.getElementById("reset");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var winingScoreDisplay = document.querySelector("p span");
@@ -37,14 +37,13 @@ p2Click.addEventListener("click", function() {
     };
   });
 
-resetClick.addEventListener("click", function() {
-  alert("in");
-  reset();
-});
+resetButton.addEventListener("click", function(){
+  	reset();
+  });
 
-function reset(){
-  p1Score = 0;
-  p2Score = 0;
-  console.log(p1Score , p2Score);
-
-};
+// function reset(){
+//   p1Score = 0;
+//   p2Score = 0;
+//   console.log(p1Score , p2Score);
+//
+// };
