6aab368d883b397f45a43052fb062bea4dcd9714
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-05 20:04:02 -0800

    Score Keeper
    Practicing with JS

---
+++ js/script(toggle color).js
@@ -0,0 +1,16 @@
+var button = document.querySelector("button");
+
+  button.addEventListener("click", function (){
+        document.body.classList.toggle("red");
+    });
+
+    // var iswhite = true;
+    // button.addEventListener("click", function (){
+    //   if( iswhite === true){
+    //     document.body.style.background = "red";
+    //     iswhite = false;
+    //   } else {
+    //     document.body.style.background = "white";
+    //     iswhite = true;
+    //   }
+    // });
--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
-var button = document.querySelector("button");
+var p1Click = document.querySelector("#p1");
+var p2Click = document.getElementById("p2");
+var p1Score = 0;
+var p2Score = 0;
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var winScore = 5;
 
-  button.addEventListener("click", function (){
-        document.body.classList.toggle("red");
-    });
 
-    // var iswhite = true;
-    // button.addEventListener("click", function (){
-    //   if( iswhite === true){
-    //     document.body.style.background = "red";
-    //     iswhite = false;
-    //   } else {
-    //     document.body.style.background = "white";
-    //     iswhite = true;
-    //   }
-    // });
+
+p1Click.addEventListener("click", function(){
+  p1Score++;
+  console.log(p1Score);
+  p1Display.textContent = p1Score;
+});
+
+
+p2Click.addEventListener("click", function() {
+      p2Score++;
+      p2Display.textContent = p2Score;
+      console.log(p2Score);
+  })
