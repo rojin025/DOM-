2343bc30d89349b3c54f5bebfd8f65d175507f21
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-05 20:17:51 -0800

    v.2.2

--- js/script.js
+++ js/script.js
@@ -5,18 +5,30 @@ var p2Score = 0;
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var winScore = 5;
-
-
+var gameOver = false;
 
 p1Click.addEventListener("click", function(){
-  p1Score++;
-  console.log(p1Score);
-  p1Display.textContent = p1Score;
+  if(!gameOver){
+    p1Score++;
+    console.log(p1Score);
+    if(p1Score === winScore) {
+      gameOver = true;
+      console.log("gameOver");
+    };
+    p1Display.textContent = p1Score;
+
+  };
 });
 
 
 p2Click.addEventListener("click", function() {
+    if(!gameOver){
       p2Score++;
       p2Display.textContent = p2Score;
       console.log(p2Score);
+      if(p2Score === winScore){
+        gameOver = true;
+        console.log("gameOver");
+      };
+    };
   })
