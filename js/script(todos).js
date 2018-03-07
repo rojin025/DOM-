1757a84fc9dd97f1877e7b9252b121e8943a9404
Rojin BIjukchhe <35796647+rojin025@users.noreply.github.com>
2018-03-07 12:00:52 -0800

    v.2
    done

--- js/script(todos).js
+++ js/script(todos).js
@@ -2,9 +2,12 @@ var Lis = document.querySelectorAll("li");
 
 for(var i = 0 ; i < Lis.length ; i ++){
   Lis[i].addEventListener("mouseover", function(){
-    this.style.color = "green";
+    this.classList.add("selected");
   });
   Lis[i].addEventListener("mouseout", function(){
-    this.style.color = "black";
+    this.classList.remove("selected");
+  });
+  Lis[i].addEventListener("click", function(){
+    this.classList.toggle("done");
   });
 };
